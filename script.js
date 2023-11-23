5f5ab509a348be2706f0c1c649b3266138068873
topclothes <151821880+topclothes@users.noreply.github.com>
2023-11-23 14:04:12 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,39 +2,6 @@ window.onload = () => {
 
 
 
-let pay = document.querySelector("#pu");
-let free = document.querySelector("#bz");
-
-
-
-pay.onchange = () => {
-
-let sumpay = document.querySelector(".sumpu p");
-sumpay.innerHTML = pay.value * 500 + "р - платные услуги";	
-
-let notb = document.querySelector(".notbonus p");
-notb.innerHTML = pay.value * 500 + free.value * 320 + "р - без премии 10к";
-
-let wb = document.querySelector(".withbonus p");
-wb.innerHTML = 10000 + pay.value * 500 + free.value * 320 + "р - с премии 10к";
-
-
-
-}
-
-
-free.onchange = () => {
-
-let sumfree = document.querySelector(".sumbz p");
-sumfree.innerHTML = free.value * 320 + "р - бесплатные замены";	
-
-let notb = document.querySelector(".notbonus p");
-notb.innerHTML = pay.value * 500 + free.value * 320+ "р - без премии 10к";
-
-let wb = document.querySelector(".withbonus p");
-wb.innerHTML = 10000 + pay.value * 500 + free.value * 320 + "р - с премии 10к";
-
-}
 
 
 }
